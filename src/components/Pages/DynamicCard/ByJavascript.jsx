90bbb7731fd6ef2ab1a16bce718dd9cdea19fe83
almubin78 <almubin78@gmail.com>
2025-07-30 23:52:34 +0600

    return previous full code for dynamic card by javascript

--- src/components/Pages/DynamicCard/ByJavascript.jsx
+++ src/components/Pages/DynamicCard/ByJavascript.jsx
@@ -3,41 +3,28 @@ import React, { useEffect, useState } from 'react';
 const ByJavascript = () => {
     const [students , setStudents ] = useState(0);
     // console.log(students.length);
-    // useEffect(()=>{
-    //     const updateCardPerPage = () =>{
-    //         if(window.matchMedia("(max-width:700px)").matches){
-    //             console.log('(max-width:700px)');
-    //             setStudents(1)
-    //         }
-    //         else if(window.matchMedia("(max-width:900px)").matches){
-    //             console.log('max-width:900px');
-    //             setStudents(2)
-    //         }
-    //         else{
-    //             console.log('No Logs');
-    //             setStudents(3)
-    //         }
-    //     }
-
-    //     //
-    //     updateCardPerPage();
-    //     window.addEventListener('nameOfListener',updateCardPerPage)
-    //     return ()=>{window.removeEventListener('nameOfListener',updateCardPerPage)}
-    // },[])
     useEffect(()=>{
-        const updateStudent = () =>{
-            if(window.matchMedia("(min-width:700px)").matches){
-                console.log('min-w:700')
-                
+        const updateCardPerPage = () =>{
+            if(window.matchMedia("(max-width:700px)").matches){
+                console.log('(max-width:700px)');
+                setStudents(1)
+            }
+            else if(window.matchMedia("(max-width:900px)").matches){
+                console.log('max-width:900px');
+                setStudents(2)
+            }
+            else{
+                console.log('No Logs');
+                setStudents(3)
             }
         }
+
         //
-        updateStudent()
-
-        window.addEventListener('nameOf',updateStudent);
-        return ()=>window.removeEventListener('nameOf',updateStudent)
-
+        updateCardPerPage();
+        window.addEventListener('nameOfListener',updateCardPerPage)
+        return ()=>{window.removeEventListener('nameOfListener',updateCardPerPage)}
     },[])
+   
     return (
         <div>
             Dynamic page for card
