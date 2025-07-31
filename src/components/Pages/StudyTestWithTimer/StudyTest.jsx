581c1829512692cb9decdb5a0f4fee3357134303
almubin78 <almubin78@gmail.com>
2025-07-31 21:08:27 +0600

    start practice for StudyTest with timer system

--- src/components/Pages/StudyTestWithTimer/StudyTest.jsx
+++ src/components/Pages/StudyTestWithTimer/StudyTest.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 
 
 const StudyTest = () => {
-   
+    const [selectedBatch,setSelectedBatch]=useState('');
+    const [batchQuestions, setBatchQuestions] = useState([]);
+    const [newTasks, setNewTasks] = useState([]);
+   const [currentStudent, setCurrentStudent] = useState(null);
     
     return (
         <div>
