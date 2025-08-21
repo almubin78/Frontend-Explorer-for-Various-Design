4c3886e407c143d844048e4e0d40fc8ee7c132d5
almubin78 <almubin78@gmail.com>
2025-08-21 22:28:32 +0600

    primary logic build for studytest root file

--- src/Pages/StudyTest/StudyTestRoot.jsx
+++ src/Pages/StudyTest/StudyTestRoot.jsx
@@ -1,6 +1,37 @@
+import { useEffect } from "react";
 
 
 const StudyTestRoot = () => {
+    useEffect(()=>{
+
+    },[])
+    //selectRandomStudent
+    const selectRandomStudent = ()=>{
+
+    }
+    //selectRandomQuestions
+    const selectRandomQuestions = ()=>{
+
+    }
+    //additional task 
+    const assignRandomTask = ()=>{
+
+    }
+    // time up for current student
+    const handleTimeUp = ()=>{
+
+    }
+    //toggle student attendance
+    const toggleStudentAttendance = ()=>{
+
+    }
+    // confirm attendance students
+    const confirmAttendanceAndStart = ()=>{
+
+    }
+    //filter out duplicate students
+    
+
     return (
         <div>
             This is StudyTest root file
--- src/socket.js
+++
@@ -1,6 +0,0 @@
-
-// src/socket.js
-import { io } from 'socket.io-client';
-
-// Connect to backend server (change to your actual backend URL)
-export const socket = io('http://localhost:5000');
