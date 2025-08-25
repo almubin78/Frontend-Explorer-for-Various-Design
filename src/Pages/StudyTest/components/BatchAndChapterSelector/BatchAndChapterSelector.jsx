360a96162605e8c205798629e5ead0d82a92a032
almubin78 <almubin78@gmail.com>
2025-08-25 07:30:02 +0600

    batch and chapter selector components create

---
+++ src/Pages/StudyTest/components/BatchAndChapterSelector/BatchAndChapterSelector.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BatchAndChapterSelector = () => {
+    return (
+        <div>
+            this is for batch and chapter selector
+        </div>
+    );
+};
+
+export default BatchAndChapterSelector;
