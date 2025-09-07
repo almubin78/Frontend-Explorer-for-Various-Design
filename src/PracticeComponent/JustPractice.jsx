040bf8b71a43cf1404a59cc073dba97e45db7c67
almubin78 <almubin78@gmail.com>
2025-09-07 23:38:20 +0600

    practice

--- src/PracticeComponent/JustPractice.jsx
+++ src/PracticeComponent/JustPractice.jsx
@@ -1,27 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { studentsData } from "../data/allStudentsData";
-import { allClassesQuestionsData } from "../data/questionsData";
+import { useState } from "react";
+
 
 const JustPractice = () => {
-  const [selectedBatch, setSelectedBatch] = useState("Ten");
+  //for batch and chapter
+  const [selectedBatch, setSelectedBatch] = useState('');
+  const [selectedChapter, setSelectedChapter] = useState('');
+  // data states
   const [batchStudents, setBatchStudents] = useState([]);
-  const [batchQuestions, setBatchQuestions] = useState([]);
-  const [presentStudents, setPresentStudents] = useState([]);
+  const [questions, setQuestions] = useState([]);
 
-  useEffect(() => {
-    if (selectedBatch && studentsData[selectedBatch]) {
-      const batchStudents = studentsData[selectedBatch];
-      const batchQuestions = allClassesQuestionsData[selectedBatch];
-      setBatchStudents(batchStudents);
-      setBatchQuestions(batchQuestions);
-      setPresentStudents(
-        batchStudents.map((student) => ({ ...student, present: true }))
-      );
-    }
-  }, [selectedBatch]);
-
-  console.log(batchQuestions, batchStudents);
-  // Function to select a random student who hasn't answered yet
   
 
   return <>this is for practice</>;
